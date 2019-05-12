ff3c7c1bbb87f643c53c4830e3227467ce8db4de
apas <apostolos.me@gmail.com>
2019-05-12 21:15:04 +0200

    makes style fixes and minor code improvements

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, redirect, send_from_directory, url_for, after_this_request, Response
 from werkzeug.utils import secure_filename
 from glob import glob
-import pypandoc
 import os
 import shutil
 import subprocess
+import pypandoc
 
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(["md", "txt", "png", "jpg", "bib", "csl"])
 app.config["UPLOAD_FOLDER"] = "uploads"
+uploads = app.config["UPLOAD_FOLDER"]
 os.environ.setdefault('PYPANDOC_PANDOC', '/home/linuxbrew/.linuxbrew/bin/pandoc')
 
 def allowed_file(filename):
@@ -16,9 +17,9 @@ def allowed_file(filename):
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def prep():
-    folder = app.config["UPLOAD_FOLDER"]
-    for file in os.listdir(folder):
-        file_path = os.path.join(folder, file)
+    for file in os.listdir(uploads):
+        file_path = os.path.join(uploads, file)
+
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
@@ -27,9 +28,9 @@ def prep():
         except Excception as e:
             print(e)
 
-def pandoc(options, file):
+def pandoc(file):
     filters = ["/home/linuxbrew/.linuxbrew/bin/pandoc-crossref",
-    "/home/linuxbrew/.linuxbrew/bin/pandoc-citeproc"]
+                "/home/linuxbrew/.linuxbrew/bin/pandoc-citeproc"]
     pdoc_args = ["--template=default.latex",
                 "-f", "markdown+raw_tex+yaml_metadata_block",
                 "-Mreference-section-title=References",
@@ -38,47 +39,48 @@ def pandoc(options, file):
                 "--pdf-engine", "/home/linuxbrew/.linuxbrew/bin/pdflatex",
                 "--highlight-style", "pygments",
                 "-V", "urlcolor=blue"]
-    print(file)
+
     file_out = "uploads/" + file.split("/", 1)[-1]
-    file_out = file_out.split(".", 1)[0]
-    file_out = file_out + ".pdf"
-    print(file_out)
+    file_out = file_out.split(".", 1)[0] + ".pdf"
+
     output = pypandoc.convert_file(file, format="md", to="latex",
                                     extra_args=pdoc_args, filters=filters,
                                     outputfile=file_out)
 
-def find_ext(dr, ext):
-    return glob(os.path.join(dr, "*.{}".format(ext)))
+def find_files(directory, extension):
+    return glob(os.path.join(directory, "*.{}".format(extension)))
 
 @app.route("/")
 def index():
    return "And all watched over by machines of loving grace." 
 
 @app.route("/<path:path>/")
-def renderurl(path):
+def url(path):
     return "And all watched over by machines of loving grace."
 
 @app.route("/md", methods=["POST"])
 def md():
     if request.method == "POST":
-        print(request.data)
-        print(request.files)
         if "file" not in request.files:
-            return redirect(request.url)
+            return "failure"
+
         file = request.files["file"]
-        print(file)
+
         if file and allowed_file(file.filename):
             prep()
             filename = secure_filename(file.filename) + ".md"
-            path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            file.save(path)
-            pandoc("-d", path)
+            file_path = os.path.join(uploads, filename)
+            file.save(file_path)
+            pandoc(file_path)
             return "success"
+
         return "pandoc"
 
 @app.route("/pdf")
 def pdf():
-    files = find_ext(app.config["UPLOAD_FOLDER"], "pdf")
+    files = find_files(uploads, "pdf")
+    file = files[0].split("/", 1)[-1]
+
     @after_this_request
     def clean(response):
         try:
@@ -86,14 +88,12 @@ def pdf():
         except Exception as e:
             print(e)
         return response
-    print(files)
-    print(files[0].split("/", 1)[-1])
-    return send_from_directory(app.config["UPLOAD_FOLDER"],
-                                files[0].split("/", 1)[-1],
-                                as_attachment=True)
+
+    return send_from_directory(uploads, file, as_attachment=True)
 
 @app.route("/robots.txt")
 def robots():
     Disallow = lambda string: "Disallow: {0}".format(string)
+
     return Response("User-agent: *\n{0}\n".format("\n".join([
         Disallow("/")])))
